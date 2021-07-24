baeae5f9c542fd6d80577923b185af35fe097d3c
norther <tomaszczekaj@MacBook-Pro-norther.local>
2021-07-24 18:46:58 +0200

    add tags and import existing resources

--- cdk_multi_enviroment/cdk_multi_enviroment_stack.py
+++ cdk_multi_enviroment/cdk_multi_enviroment_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import core as cdk
 import aws_cdk.aws_s3 as _s3
+import aws_cdk.aws_ec2 as _ec2
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -12,11 +13,19 @@ class CdkMultiEnviromentStack(cdk.Stack):
 
     def __init__(self, scope: cdk.Construct, construct_id: str, is_prod=False, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        prod_confs = self.node.try_get_context('envs')['dev']
+        print(prod_confs)
         # The code that defines your stack goes here
         if is_prod:
-            bucket = _s3.Bucket(self, "MyProdBucket",
-            versioned = True
-            )
+            vpc = _ec2.Vpc(self, "MyVPCPROD",
+                           cidr="10.0.0.0/16"
+                           )
+            tag_vpc = core.Tags.of(vpc).add("Owner", "Tomasz Czekaj")
         else:
-            bucket = _s3.Bucket(self, "MyDevBucket")
+            vpc = _ec2.Vpc(self, "MyVPCDEV",
+                           cidr="10.0.0.0/16"
+                           )
+        # Importing existing resources
+        # bucket_imported = _s3.Bucket.from_bucket_name(self, "MyImportedBucket",
+        #                             "bucket_name_to_import"
+        #                             )
